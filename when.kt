58c2499bf6fdfc97277ae60b6763a5d2364081a0
DannyRamadhan81 <107790220+DannyRamadhan81@users.noreply.github.com>
2022-07-25 10:28:41 +0700

    Create when.kt

---
+++ when.kt
@@ -0,0 +1,16 @@
+fun main() {
+ 
+
+ val angka = 10
+    val nilaiangka = when (angka){
+	6 -> "nilai 6"
+    7 -> "nilai 7"
+    8 -> "niali 8"
+    else -> "nilai tidak ada"
+    }
+    println(nilaiangka)
+
+ 
+ 
+ 
+}
